33ae52343838110ba846426481cebd3ea46642c9
Tom Xu <thomas.w.xu@gmail.com>
2024-01-27 13:54:52 -0500

    Move player script to correct directory

--- AdventuresInScramble_GD/Scripts/world.gd
+++ AdventuresInScramble_GD/Scripts/world.gd
@@ -5,7 +5,6 @@ var obstacle_scene = preload("res://Scenes/obstacle.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	spawn_obstacle()
-	print("SPAWNED OBSTACLE")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
