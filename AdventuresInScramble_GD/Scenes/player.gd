48a3f4476f8be70436c0e406bd155d0cf707737b
Tom Xu <thomas.w.xu@gmail.com>
2024-01-27 13:06:23 -0500

    Add fast fall and basic obstacle

--- AdventuresInScramble_GD/Scenes/player.gd
+++ AdventuresInScramble_GD/Scenes/player.gd
@@ -1,8 +1,9 @@
 extends CharacterBody2D
 
 # Constants
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 500.0
+const JUMP_VELOCITY = -600.0
+const FAST_FALL_VELOCITY = 2000
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity") # 980, dir (0, 1)
@@ -15,6 +16,9 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+
+	if Input.is_action_pressed("fast_fall") and not is_on_floor():
+		velocity.y = FAST_FALL_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
---
+++ AdventuresInScramble_GD/Scripts/obstacle.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	randomize()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_body_entered(body):
+	if "Player" in body.name:
+		print("player entered")
