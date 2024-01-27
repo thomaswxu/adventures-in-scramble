7582545505e90d1db4171b3c5046968f419936b9
Tom Xu <thomas.w.xu@gmail.com>
2024-01-27 14:22:42 -0500

    Add multiple random object spawns

--- AdventuresInScramble_GD/Scripts/obstacle.gd
+++ AdventuresInScramble_GD/Scripts/obstacle.gd
@@ -1,7 +1,8 @@
 extends Area2D
 
 const FALL_GRAVITY = 1000
-const LAUNCH_VELOCITY = 1000
+const LAUNCH_SPEED = 1000
+const KILL_HEIGHT = 1000
 
 var player = null
 var velocity = Vector2(0.0, 0.0)
@@ -15,27 +16,30 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	position += velocity * delta
-	velocity += Vector2(0, FALL_GRAVITY * delta)
+	velocity.y += FALL_GRAVITY * delta
+	
+	if position.y > KILL_HEIGHT:
+		queue_free()
 
 # Spawn with random:
 # - Position relative to the player
 # - Initial velocity
-func spawn():	
+func spawn():
 	# Randomize starting position offsets
 	var spawn_on_left = randi_range(0, 1)
-	var x_offset = 500
+	var x_offset = 1000
 	if (spawn_on_left):
 		x_offset *= -1
-	var y_offset = randf_range(300, 700)
+	var y_offset = randf_range(-300, -700)
 	position = player.position + Vector2(x_offset, y_offset)
 
 	# Randomize initial velocity
 	var initial_velocity_angle = 0.0
 	if (spawn_on_left):
-		initial_velocity_angle = randf_range(0, PI / 2.0)
+		initial_velocity_angle = randf_range(0, -PI / 2.0)
 	else:
-		initial_velocity_angle = randf_range(PI / 2.0, PI)
-	velocity = LAUNCH_VELOCITY * Vector2(cos(initial_velocity_angle), sin(initial_velocity_angle))
+		initial_velocity_angle = randf_range(-PI / 2.0, -PI)
+	velocity = LAUNCH_SPEED * Vector2(cos(initial_velocity_angle), sin(initial_velocity_angle))
 	print("Obstacle spawned, velocity = (" + str(velocity[0]) + ", " + str(velocity[1]) + ")")
 
 func _on_body_entered(body):
--- AdventuresInScramble_GD/Scripts/world.gd
+++ AdventuresInScramble_GD/Scripts/world.gd
@@ -1,10 +1,15 @@
 extends Node2D
 
+const MIN_OBSTACLE_SPAWN_DELAY_s = 0.5
+const MAX_OBSTACLE_SPAWN_DELAY_s = 2.0
+
 var obstacle_scene = preload("res://Scenes/obstacle.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	spawn_obstacle()
+	randomize()
+	$ObstacleSpawnTimer.wait_time = get_obstacle_spawn_timeout()
+	$ObstacleSpawnTimer.start()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -13,3 +18,12 @@ func _process(delta):
 func spawn_obstacle():
 	var obstacle_instance = obstacle_scene.instantiate()
 	add_child(obstacle_instance)
+
+func _on_obstacle_spawn_timer_timeout():
+	spawn_obstacle()
+	$ObstacleSpawnTimer.wait_time = get_obstacle_spawn_timeout()
+	#print("Changed obstacle spawn timer wait time to: " + str($ObstacleSpawnTimer.wait_time))
+
+# Get a random obstacle spawn timeout, within the min/max parameters above
+func get_obstacle_spawn_timeout() -> float:
+	return randf_range(MIN_OBSTACLE_SPAWN_DELAY_s, MAX_OBSTACLE_SPAWN_DELAY_s)
